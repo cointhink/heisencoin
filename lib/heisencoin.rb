3437f48c7d7a8ed2285d2a20b1763f1ca48af8a9
Don Park <don@donpark.org>
2013-03-21 13:59:15 -0700

    bootstrap the libs

--- lib/heisencoin.rb
+++ lib/heisencoin.rb
@@ -1,10 +1,22 @@
 require 'active_record'
 require 'models/strategy'
+require 'zmq'
 
 APP_ROOT = File.dirname(__FILE__)+"/../"
 
-# Load database.yml
-db_settings = YAML.load_file(APP_ROOT+"/config/database.yml")
+module Heisencoin
+  def self.setup
+    # Load database.yml
+    db_settings = YAML.load_file(APP_ROOT+"/config/settings.yml")
 
-# Connect ActiveRecord
-ActiveRecord::Base.establish_connection(db_settings["development"])
+    # Connect ActiveRecord
+    ActiveRecord::Base.establish_connection(db_settings["development"])
+
+    # Listen on ZeroMQ
+    context = ZMQ::Context.new(1)
+    inbound = context.socket(ZMQ::UPSTREAM)
+    listening = db_settings["zeromq"]["listen"]
+    puts "zeromq listening on #{listening}"
+    inbound.bind(listening)
+  end
+end
