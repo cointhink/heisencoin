70951d1a8439f9867abae45e745d363a37985a8f
Don Park <donp@donp.org>
2013-09-06 10:10:04 -0700

    start some arbitrage functions

--- lib/heisencoin/arbitrage.rb
+++ lib/heisencoin/arbitrage.rb
@@ -1,4 +1,11 @@
 module Heisencoin
   class Arbitrage
+    def add_exchanges(exchanges)
+      puts "adding #{exchanges.size} exchanges"
+    end
+
+    def plan
+      puts "formulating a plan"
+    end
   end
 end
