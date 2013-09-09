3d4109aaa954081d1c8e9e9168133b50cb8ba242
Don Park <donp@donp.org>
2013-09-09 11:13:08 -0700

    load all rb files

--- lib/heisencoin.rb
+++ lib/heisencoin.rb
@@ -1,4 +1,5 @@
-Dir['lib/heisencoin/*.rb'].map{|f| File.basename(f,".rb")}
+LIBDIR = File.dirname(__FILE__)
+Dir[LIBDIR+'/heisencoin/*.rb'].map{|f| File.basename(f,".rb")}
                           .each{|rb| require "heisencoin/#{rb}"}
 
 module Heisencoin
