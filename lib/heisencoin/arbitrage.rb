d01d99b7b20c89754c1d59887862e235cc9ee0be
Don Park <donp@donp.org>
2013-09-09 09:48:34 -0700

    sorted insert

--- lib/heisencoin/arbitrage.rb
+++ lib/heisencoin/arbitrage.rb
@@ -1,17 +1,28 @@
 module Heisencoin
   class Arbitrage
-    attr_accessor :exchanges
+    attr_accessor :exchanges, :bids, :asks
 
     def initialize
       @exchanges = []
+      @asks = Market.new
+      @bids = Market.new
     end
 
     def add_exchanges(exchanges)
       @exchanges += exchanges
+      exchanges.each do |exchange|
+        add_depth(exchange)
+      end
+    end
+
+    def add_depth(exchange)
+      @asks.import(exchange, exchange.depth["asks"])
+      @asks.import(exchange, exchange.depth["bids"])
     end
 
     def plan
       puts "formulating a plan"
+
     end
   end
 end
--- tests/arbitrage.rb
+++ tests/arbitrage.rb
@@ -5,6 +5,9 @@ class TestMeme < Minitest::Test
   describe Heisencoin::Arbitrage do
     before do
       @arby = Heisencoin::Arbitrage.new
+      @ex1 = Heisencoin::Exchange.new({name:'btcx',
+          depth: {"asks" => [], "bids" => []}
+        })
     end
 
     describe "when setting up the list of exchanges" do
@@ -13,11 +16,25 @@ class TestMeme < Minitest::Test
       end
 
       it "must accept new exchanges" do
-        exchanges = [Heisencoin::Exchange.new({name:'btcx'})]
+        exchanges = [@ex1]
         @arby.add_exchanges(exchanges)
         @arby.exchanges.size.must_equal 1
       end
     end
+
+    describe "when importing offers" do
+      it "must keep them sorted" do
+        @ex1.depth["asks"] += [ [10,1],
+                                [11,1,1],
+                                [9,0.9] ]
+        @arby.add_exchanges([@ex1])
+        puts @arby.asks.offers.inspect
+        @arby.asks.offers.length.must_equal 3
+        @arby.asks.offers.first[1].must_equal 9
+
+      end
+    end
+
   end
 
 end
