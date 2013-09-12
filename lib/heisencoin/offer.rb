709578a2536b25a81b474db821a977dd72ddbaec
Don Park <donp@donp.org>
2013-09-12 11:56:06 -0700

    offer factory

--- lib/heisencoin/offer.rb
+++ lib/heisencoin/offer.rb
@@ -3,9 +3,11 @@ module Heisencoin
     attr_accessor :exchange, :price, :quantity
 
     def self.from_array(exchange, raw_offer)
-      @exchange = exchange
-      @price = raw_offer[0]
-      @quantity = raw_offer[1]
+      offer = Offer.new
+      offer.exchange = exchange
+      offer.price = raw_offer[0]
+      offer.quantity = raw_offer[1]
+      offer
     end
 
   end
